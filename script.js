62be904ac36afca1cabdf24f14d48cfdc4efdeea
Diego Martins <diegohenrique87@gmail.com>
2023-07-07 18:24:30 -0300

    adaptacao do exemplo da sala para o trabalho

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-function decimalParaBinario(decimal) {
-    return parseInt(decimal).toString(2);
+function decimalParaBinario(dec){
+  return parseInt(dec).toString(2);
+}
+
+function decimalParaHexadecimal(dec) {
+  return parseInt(dec).toString(16).toUpperCase();
+}
+
+function binarioParaDecimal(bin) {
+  var dec = 0;
+  var pos = 0;
+
+  while (bin > 0) {
+      var digito = bin % 10;
+      dec += digito * Math.pow(2, pos);
+      bin = Math.floor(bin / 10);
+      pos++;
   }
-  
-  function binarioParaDecimal(binario) {
-    var decimal = 0;
-    var posicao = 0;
-  
-    while (binario > 0) {
-      var digito = binario % 10;
-      decimal += digito * Math.pow(2, posicao);
-      binario = Math.floor(binario / 10);
-      posicao++;
-    }
-  
-    return decimal;
-  }
-  
-  function decimalParaHexadecimal(decimal) {
-    return parseInt(decimal).toString(16).toUpperCase();
-  }
-  
-  function hexadecimalParaDecimal(hexadecimal) {
-    return parseInt(hexadecimal, 16);
-  }
-  
+
+  return dec;
+}
+
+function hexadecimalParaDecimal(hex) {
+  return parseInt(hex, 16);
+}
+
+function copiar(componente){
+  var copiar = document.getElementById(componente).textContent.split(':')[1].trim();
+  navigator.clipboard.writeText(copiar);
+}
+
+function copiar2(componente){
+  var copiar2 = document.getElementById(componente).value;
+  navigator.clipboard.writeText(copiar2);
+}
